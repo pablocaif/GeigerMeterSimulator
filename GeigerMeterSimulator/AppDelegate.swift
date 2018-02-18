2fde66325735bd0ed52d876d42c00d4d7689f183
Pablo Caif <pablocaif@gmail.com>
2018-02-18 18:55:30 +1100

    Initial Commit

---
+++ GeigerMeterSimulator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GeigerMeterSimulator
+//
+//  Created by Pablo Caif on 18/2/18.
+//  Copyright © 2018 Pablo Caif. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GeigerMeterSimulator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GeigerMeterSimulator
+//
+//  Created by Pablo Caif on 18/2/18.
+//  Copyright © 2018 Pablo Caif. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
